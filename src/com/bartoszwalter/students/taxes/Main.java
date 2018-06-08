b70d737bdbf10f627749615fb23415ff1c0e6c50
jcofta <jan.cofta@gmail.com>
2018-06-08 09:11:56 +0200

    Ekstrakcja do metod

---
+++ src/com/bartoszwalter/students/taxes/Main.java
@@ -0,0 +1,8 @@
+package com.bartoszwalter.students.taxes;
+
+public class Main {
+    public static void main(String[] args) {
+        TaxCalculator taxCalculator = new TaxCalculator();
+        taxCalculator.run();
+    }
+}
