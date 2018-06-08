6798b5bfbb34d96bd8e46669eb19814f0bdc18f5
jcofta <jan.cofta@gmail.com>
2018-06-08 09:31:08 +0200

    Refaktor

--- src/com/bartoszwalter/students/taxes/TaxCalculator.java
+++ src/com/bartoszwalter/students/taxes/TaxCalculator.java
@@ -3,10 +3,10 @@ package com.bartoszwalter.students.taxes;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.text.DecimalFormat;
+import java.util.HashMap;
 
 public class TaxCalculator {
 
-    public double podstawa = 0;
     public char umowa = ' ';
     // składki na ubezpieczenia społeczne
     public double s_emerytalna = 0; // 9,76% podstawyy
@@ -19,14 +19,17 @@ public class TaxCalculator {
     public double zaliczkaNaPod = 0; // zaliczka na podatek dochodowy 18%
     public double kwotaZmiejsz = 46.33; // kwota zmienjszająca podatek 46,33 PLN
     public double zaliczkaUS = 0;
-    public double zaliczkaUS0 = 0;
+    public double zaliczkaUSZaokr = 0;
     public double oPodstawa = 0;
     double podstawaOpodat = 0;
-    double podstawaOpodat0 = 0;
+    double podstawaOpodatZaokr = 0;
     double podatekPotracony = 0;
     double wynagrodzenie = 0;
+    DecimalFormat df00 = new DecimalFormat("#.00");
+    DecimalFormat df = new DecimalFormat("#");
 
     public void run() {
+        Double podstawa = 0.0;
         try {
             InputStreamReader isr = new InputStreamReader(System.in);
             BufferedReader br = new BufferedReader(isr);
@@ -43,19 +46,16 @@ public class TaxCalculator {
             return;
         }
 
-        DecimalFormat df00 = new DecimalFormat("#.00");
-        DecimalFormat df = new DecimalFormat("#");
-
         if (umowa == 'P') {
-            obliczUOP(df00, df);
+            obliczUOP(podstawa);
         } else if (umowa == 'Z') {
-            obliczUZ(df00, df);
+            obliczUZ(podstawa);
         } else {
             System.out.println("Nieznany typ umowy!");
         }
     }
 
-    private void wypiszSkladki(DecimalFormat df00, DecimalFormat df) {
+    private void wypiszSkladki(Double podstawa) {
         System.out.println("Podstawa wymiaru składek " + podstawa);
         oPodstawa = obliczonaPodstawa(podstawa);
         System.out.println("Składka na ubezpieczenie emerytalne "
@@ -72,29 +72,47 @@ public class TaxCalculator {
                 + df00.format(s_zdrow1) + " 7,75% = " + df00.format(s_zdrow2));
     }
 
-    private void obliczUZ(DecimalFormat df00, DecimalFormat df) {
-
-        wypiszSkladki(df00, df);
+    private void obliczUZ(Double podstawa) {
+        wypiszSkladki(podstawa);
         kwotaZmiejsz = 0;
         kosztyUzyskania = (oPodstawa * 20) / 100;
         podstawaOpodat = oPodstawa - kosztyUzyskania;
-        podstawaOpodat0 = Double.parseDouble(df.format(podstawaOpodat));
-        obliczPodatek(podstawaOpodat0);
+        podstawaOpodatZaokr = Double.parseDouble(df.format(podstawaOpodat));
+        zaliczkaNaPod = obliczPodatek(podstawaOpodatZaokr);
         podatekPotracony = zaliczkaNaPod;
-        obliczZaliczke();
-        zaliczkaUS0 = Double.parseDouble(df.format(zaliczkaUS));
-        wynagrodzenie = podstawa
-                - ((s_emerytalna + s_rentowa + u_chorobowe) + s_zdrow1 + zaliczkaUS0);
+        zaliczkaUS = obliczZaliczke();
+        zaliczkaUSZaokr = Double.parseDouble(df.format(zaliczkaUS));
+        wynagrodzenie = podstawa - ((s_emerytalna + s_rentowa + u_chorobowe) + s_zdrow1 + zaliczkaUSZaokr);
 
-        wypiszUZ(df00, df);
+        wypiszUZ();
     }
 
-    private void wypiszUoP(DecimalFormat df00, DecimalFormat df) {
+    private void obliczUOP(Double podstawa) {
+        HashMap<String, Double> skladowePlacy = new HashMap<>();
+        wypiszSkladki(podstawa);
+        podstawaOpodat = oPodstawa - kosztyUzyskania;
+        skladowePlacy.put("PodstawaOpodatkowania", podstawaOpodat);
+        podstawaOpodatZaokr = Double.parseDouble(df.format(podstawaOpodat));
+        skladowePlacy.put("PodstawaOpodatkowaniaZaokraglona", podstawaOpodatZaokr);
+        zaliczkaNaPod = obliczPodatek(podstawaOpodatZaokr);
+        skladowePlacy.put("ZaliczkaNaPodatek", zaliczkaNaPod);
+        podatekPotracony = zaliczkaNaPod - kwotaZmiejsz;
+        skladowePlacy.put("PodatekPotracony", podatekPotracony);
+        zaliczkaUS = obliczZaliczke();
+        skladowePlacy.put("ZaliczkaUS", zaliczkaUS);
+        zaliczkaUSZaokr = Double.parseDouble(df.format(zaliczkaUS));
+        skladowePlacy.put("ZaliczkaUSZaokraglona", zaliczkaUSZaokr);
+        wynagrodzenie = podstawa - ((s_emerytalna + s_rentowa + u_chorobowe) + s_zdrow1 + zaliczkaUSZaokr);
+
+        wypiszUoP(podstawa);
+    }
+
+    private void wypiszUoP(Double podstawa) {
         System.out.println("UMOWA O PRACĘ");
         System.out.println("Koszty uzyskania przychodu w stałej wysokości "
                 + kosztyUzyskania);
         System.out.println("Podstawa opodatkowania " + podstawaOpodat
-                + " zaokrąglona " + df.format(podstawaOpodat0));
+                + " zaokrąglona " + df.format(podstawaOpodatZaokr));
         System.out.println("Zaliczka na podatek dochodowy 18 % = "
                 + zaliczkaNaPod);
         System.out.println("Kwota wolna od podatku = " + kwotaZmiejsz);
@@ -102,54 +120,38 @@ public class TaxCalculator {
                 + df00.format(podatekPotracony));
         System.out.println("Zaliczka do urzędu skarbowego = "
                 + df00.format(zaliczkaUS) + " po zaokrągleniu = "
-                + df.format(zaliczkaUS0));
+                + df.format(zaliczkaUSZaokr));
         System.out.println();
         System.out
                 .println("Pracownik otrzyma wynagrodzenie netto w wysokości = "
                         + df00.format(wynagrodzenie));
     }
 
-    private void wypiszUZ(DecimalFormat df00, DecimalFormat df) {
+    private void wypiszUZ() {
         System.out.println("UMOWA-ZLECENIE");
         System.out.println("Koszty uzyskania przychodu (stałe) "
                 + kosztyUzyskania);
         System.out.println("Podstawa opodatkowania " + podstawaOpodat
-                + " zaokrąglona " + df.format(podstawaOpodat0));
+                + " zaokrąglona " + df.format(podstawaOpodatZaokr));
         System.out.println("Zaliczka na podatek dochodowy 18 % = "
                 + zaliczkaNaPod);
         System.out.println("Podatek potrącony = "
                 + df00.format(podatekPotracony));
         System.out.println("Zaliczka do urzędu skarbowego = "
                 + df00.format(zaliczkaUS) + " po zaokrągleniu = "
-                + df.format(zaliczkaUS0));
+                + df.format(zaliczkaUSZaokr));
         System.out.println();
         System.out
                 .println("Pracownik otrzyma wynagrodzenie netto w wysokości = "
                         + df00.format(wynagrodzenie));
     }
 
-    private void obliczUOP(DecimalFormat df00, DecimalFormat df) {
-        wypiszSkladki(df00, df);
-        podstawaOpodat = oPodstawa - kosztyUzyskania;
-        podstawaOpodat0 = Double
-                .parseDouble(df.format(podstawaOpodat));
-        obliczPodatek(podstawaOpodat0);
-        podatekPotracony = zaliczkaNaPod - kwotaZmiejsz;
-        obliczZaliczke();
-        zaliczkaUS0 = Double.parseDouble(df.format(zaliczkaUS));
-        wynagrodzenie = podstawa
-                - ((s_emerytalna + s_rentowa + u_chorobowe) + s_zdrow1 + zaliczkaUS0);
-
-        wypiszUoP(df00, df);
+    public Double obliczZaliczke() {
+        return zaliczkaNaPod - s_zdrow2 - kwotaZmiejsz;
     }
 
-
-    public void obliczZaliczke() {
-        zaliczkaUS = zaliczkaNaPod - s_zdrow2 - kwotaZmiejsz;
-    }
-
-    public void obliczPodatek(double podstawa) {
-        zaliczkaNaPod = (podstawa * 18) / 100;
+    public Double obliczPodatek(double podstawa) {
+        return (podstawa * 18) / 100;
     }
 
     public double obliczonaPodstawa(double podstawa) {
