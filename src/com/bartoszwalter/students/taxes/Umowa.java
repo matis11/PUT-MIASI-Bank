b5da78a38938b4b00cbca8122e77abd3c9bae0d9
Paweł Szudrowicz <pablo.szudrowicz@gmail.com>
2018-06-14 21:38:53 +0200

    Umowa abstract

---
+++ src/com/bartoszwalter/students/taxes/Umowa.java
@@ -0,0 +1,56 @@
+package com.bartoszwalter.students.taxes;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+
+public abstract class Umowa {
+    public abstract void obliczKoszty(double podstawa);
+    public abstract void wypiszKoszty();
+    public abstract double obliczZaliczke();
+
+    public double SkladkaEmerytalna = 0; // 9,76% podstawyy
+    public double SkladkaRentowa = 0; // 1,5% podstawy
+    public double UbezpChorobowe = 0; // 2,45% podstawy
+    public double SkladkaZdrowotna1 = 0; // od podstawy wymiaru 9%
+    public double SkladkaZdrowotna2 = 0; // od podstawy wymiaru 7,75 %
+    public double oPodstawa = 0;
+    public double kwotaZmiejszajacaPodatek = 46.33; // kwota zmienjszająca podatek 46,33 PLN
+
+    public double obliczonaPodstawa(double podstawa) {
+        SkladkaEmerytalna = (podstawa * 9.76) / 100;
+        SkladkaRentowa = (podstawa * 1.5) / 100;
+        UbezpChorobowe = (podstawa * 2.45) / 100;
+        return (podstawa - SkladkaEmerytalna - SkladkaRentowa - UbezpChorobowe);
+    }
+
+    public void obliczUbezpieczenia(double podstawa) {
+        SkladkaZdrowotna1 = (podstawa * 9) / 100;
+        SkladkaZdrowotna2 = (podstawa * 7.75) / 100;
+    }
+
+    private void wypiszSkladki(Double podstawa) {
+        DecimalFormat df00 = new DecimalFormat("#.00");
+        DecimalFormat df = new DecimalFormat("#");
+
+        System.out.println("Podstawa wymiaru składek " + podstawa);
+        oPodstawa = obliczonaPodstawa(podstawa);
+        System.out.println("Składka na ubezpieczenie emerytalne "
+                + df00.format(SkladkaEmerytalna));
+        System.out.println("Składka na ubezpieczenie rentowe    "
+                + df00.format(SkladkaRentowa));
+        System.out.println("Składka na ubezpieczenie chorobowe  "
+                + df00.format(UbezpChorobowe));
+        System.out
+                .println("Podstawa wymiaru składki na ubezpieczenie zdrowotne: "
+                        + oPodstawa);
+        obliczUbezpieczenia(oPodstawa);
+        System.out.println("Składka na ubezpieczenie zdrowotne: 9% = "
+                + df00.format(SkladkaZdrowotna1) + " 7,75% = " + df00.format(SkladkaZdrowotna2));
+    }
+
+    public Double obliczPodatek(double podstawa) {
+        return (podstawa * 18) / 100;
+    }
+
+
+}
---
+++ src/com/bartoszwalter/students/taxes/UmowaZlecenie.java
@@ -0,0 +1,17 @@
+package com.bartoszwalter.students.taxes;
+
+import java.util.HashMap;
+
+public class UmowaZlecenie extends Umowa {
+    public void obliczKoszty(double podstawa) {
+
+    }
+
+    public void wypiszKoszty() {
+
+    }
+
+    public double obliczZaliczke() {
+        return 0.0;
+    }
+}
